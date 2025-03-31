1d265985c54d98d1fac0c3cecadec2b2b677aa63
Kings <963987632@qq.com>
2025-03-31 16:17:02 +0800

    fix

---
+++ docs/.vuepress/layouts/Layout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { usePageData, usePageFrontmatter } from "vuepress/client";
+
+import CommonWrapper from "@theme-hope/components/CommonWrapper";
+import HomePage from "@theme-hope/components/HomePage";
+import NormalPage from "@theme-hope/components/NormalPage";
+import SkipLink from "@theme-hope/components/SkipLink";
+import { FadeSlideY } from "@theme-hope/components/transitions/index";
+
+import type { ThemePageFrontmatter } from "vuepress-theme-hope";
+
+const frontmatter = usePageFrontmatter<ThemePageFrontmatter>();
+const page = usePageData();
+</script>
+<template>
+  <SkipLink />
+  <CommonWrapper>
+    <template #default>
+      <HomePage v-if="frontmatter.home" />
+      <FadeSlideY v-else>
+        <NormalPage :key="page.path" />
+      </FadeSlideY>
+    </template>
+  </CommonWrapper>
+</template>
